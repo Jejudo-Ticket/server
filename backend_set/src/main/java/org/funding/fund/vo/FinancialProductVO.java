8b822a44d00ef02ddd666a584fe381c6f315913f
joonyee <ssugjoon@skuniv.ac.kr>
2025-07-17 10:39:31 +0900

    Refactor : 펀딩 관련 VO 변수 수정

--- backend_set/src/main/java/org/funding/fund/vo/FinancialProductVO.java
+++ backend_set/src/main/java/org/funding/fund/vo/FinancialProductVO.java
@@ -1,18 +1,15 @@
 package org.funding.fund.vo;
 
+import lombok.Data;
 import org.funding.fund.vo.enumType.FundType;
 
 import java.math.BigDecimal;
 
+@Data
 public class FinancialProductVO {
     private Long productId; // 상품 id
     private String name; // 상품 이름
     private String detail; // 상품 내용
     private FundType fundType; // ENUM: "저축", "대출", "챌린지", "기부"
 
-    // 하위 타입 포함 (선택)
-    private SavingsVO savings;
-    private LoanVO loan;
-    private ChallengeVO challenge;
-    private DonationVO donation;
 }
